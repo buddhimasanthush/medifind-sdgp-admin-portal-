2b38ab34e2020b42666b484d805fc29ad4e4a794
buddhimasanthush <buddhimasanthush@gmail.com>
2026-03-21 07:14:30 +0530

    Implement Express application configuration

---
+++ artifacts/api-server/src/app.ts
@@ -0,0 +1,15 @@
+import express, { type Express } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "./routes";
+
+const app: Express = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+export default app;
